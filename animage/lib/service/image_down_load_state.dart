d8dc48014f5cdd16c64bb9c3967e1a18bd54d15d
duyphuong5126 <duyphuong5126@gmail.com>
2022-08-05 22:01:43 +0700

    Add pending ID list. Use post ID instead of file url

--- animage/lib/service/image_down_load_state.dart
+++ animage/lib/service/image_down_load_state.dart
@@ -2,9 +2,7 @@ import 'package:animage/feature/ui_model/download_state.dart';
 
 class ImageDownloadState {
   final int postId;
-  final String url;
   final DownloadState state;
 
-  const ImageDownloadState(
-      {required this.postId, required this.url, required this.state});
+  const ImageDownloadState({required this.postId, required this.state});
 }
