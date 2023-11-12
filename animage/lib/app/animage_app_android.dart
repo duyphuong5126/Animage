e7010c18e1ad4130618ca5f1fdeeea11a8a8ebc7
LaurenceNguyen <duyphuong5126@gmail.com>
2023-11-12 14:56:45 +0700

    Release 1.0.6
    - Bump app version
    - Use bounce scroll physics for android 12 and above

--- animage/lib/app/animage_app_android.dart
+++ animage/lib/app/animage_app_android.dart
@@ -7,7 +7,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class AnimageAppAndroid extends StatefulWidget {
-  const AnimageAppAndroid({Key? key}) : super(key: key);
+  const AnimageAppAndroid({Key? key, required this.isFromAndroid31})
+      : super(key: key);
+
+  final bool isFromAndroid31;
 
   @override
   State<AnimageAppAndroid> createState() => _AnimageAppAndroidState();
@@ -23,72 +26,83 @@ class _AnimageAppAndroidState extends State<AnimageAppAndroid> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            brightness: Brightness.light,
-            backgroundColor: Colors.white,
-            shadowColor: Colors.grey[400],
-            primaryColor: Colors.grey[900],
-            // grey[400]
-            textTheme: _getTextTheme(context, false)),
-        darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            backgroundColor: Colors.black,
-            shadowColor: Colors.white,
-            primaryColor: Colors.white,
-            textTheme: _getTextTheme(context, true)),
-        themeMode: ThemeMode.system,
-        routes: {
-          '/': (context) => const HomePageAndroid(),
-          detailsPageRoute: (context) => const PostDetailsPageAndroid(),
-          viewOriginalPageRoute: (context) =>
-              const ViewOriginalImagePageAndroid()
-        },
-        navigatorObservers: [
-          FirebaseAnalyticsObserver(
-              analytics: FirebaseAnalytics.instance,
-              nameExtractor: (RouteSettings settings) {
-                String? routeName = settings.name;
-                switch (settings.name) {
-                  case detailsPageRoute:
-                    routeName = postDetailsPage;
-                    break;
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: widget.isFromAndroid31
+          ? ScrollConfiguration.of(context).copyWith(
+              physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
+            )
+          : null,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.grey[400],
+        primaryColor: Colors.grey[900],
+        // grey[400]
+        textTheme: _getTextTheme(context, false),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.black,
+        shadowColor: Colors.white,
+        primaryColor: Colors.white,
+        textTheme: _getTextTheme(context, true),
+      ),
+      themeMode: ThemeMode.system,
+      routes: {
+        '/': (context) => const HomePageAndroid(),
+        detailsPageRoute: (context) => const PostDetailsPageAndroid(),
+        viewOriginalPageRoute: (context) => const ViewOriginalImagePageAndroid()
+      },
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(
+          analytics: FirebaseAnalytics.instance,
+          nameExtractor: (RouteSettings settings) {
+            String? routeName = settings.name;
+            switch (settings.name) {
+              case detailsPageRoute:
+                routeName = postDetailsPage;
+                break;
 
-                  case viewOriginalPageRoute:
-                    routeName = viewOriginalPage;
-                    break;
+              case viewOriginalPageRoute:
+                routeName = viewOriginalPage;
+                break;
 
-                  case '/':
-                    routeName = home;
-                    break;
-                }
-                return routeName;
-              }),
-        ]);
+              case '/':
+                routeName = home;
+                break;
+            }
+            return routeName;
+          },
+        ),
+      ],
+    );
   }
 
   TextTheme _getTextTheme(BuildContext context, bool isDark) {
     return Theme.of(context).textTheme.copyWith(
-        headline4: Theme.of(context)
-            .textTheme
-            .headline4
-            ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
-        headline5: Theme.of(context)
-            .textTheme
-            .headline5
-            ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
-        headline6: Theme.of(context)
-            .textTheme
-            .headline6
-            ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
-        bodyText1: Theme.of(context)
-            .textTheme
-            .bodyText1
-            ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
-        bodyText2: Theme.of(context)
-            .textTheme
-            .bodyText2
-            ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]));
+          headline4: Theme.of(context)
+              .textTheme
+              .headline4
+              ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
+          headline5: Theme.of(context)
+              .textTheme
+              .headline5
+              ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
+          headline6: Theme.of(context)
+              .textTheme
+              .headline6
+              ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
+          bodyText1: Theme.of(context)
+              .textTheme
+              .bodyText1
+              ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
+          bodyText2: Theme.of(context)
+              .textTheme
+              .bodyText2
+              ?.copyWith(color: isDark ? Colors.white : Colors.grey[900]),
+        );
   }
 
   void _initNotificationSettings() async {
@@ -97,7 +111,9 @@ class _AnimageAppAndroidState extends State<AnimageAppAndroid> {
 
     const InitializationSettings initializationSettings =
         InitializationSettings(
-            android: initializationSettingsAndroid, macOS: null);
+      android: initializationSettingsAndroid,
+      macOS: null,
+    );
 
     final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
         FlutterLocalNotificationsPlugin();
--- animage/lib/main.dart
+++ animage/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:animage/app/animage_app_ios.dart';
 import 'package:animage/domain/entity/gallery_level.dart';
 import 'package:animage/domain/use_case/check_artist_list_changed_use_case.dart';
 import 'package:animage/domain/use_case/sync_artist_list_use_case.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
@@ -27,5 +28,15 @@ void main() async {
     await syncArtistListUseCase.execute().onError((error, stackTrace) => null);
   }
 
-  runApp(Platform.isIOS ? const AnimageAppIOS() : const AnimageAppAndroid());
+  runApp(
+    Platform.isIOS
+        ? const AnimageAppIOS()
+        : AnimageAppAndroid(isFromAndroid31: await _isFromAndroid31()),
+  );
+}
+
+Future<bool> _isFromAndroid31() async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+  return androidInfo.version.sdkInt >= 31;
 }
